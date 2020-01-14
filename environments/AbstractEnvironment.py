9f569b587dec357aea6823b4c4bf3705fc5a80a1
Eduardo Di Santi <eduardodisanti@yahoo.co.uk>
2020-01-14 17:36:55 +0100

    IMPLEMENT -- TRAIN TRADER WITH Q-LEARNING MODIFY -- Enviroment to consifer inflation and change rewards

--- environments/AbstractEnvironment.py
+++ environments/AbstractEnvironment.py
@@ -1,27 +1,23 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
-
-action_SELL = 1
-action_HOLD = 0
-action_BUY  = -1
-
 class AbstractEnvironment(metaclass=ABCMeta):
 
     def __init__(self, *args, **kwargs):
         self.queries = 0
 
-        self.action_space = np.array([action_SELL, action_HOLD, action_BUY])
+        self.action_space = None
         self.done = False
 
-        _OPEN = _CLOSE = _HIGH = _LOW = _VOLUME = _MONEY = 0
-        self.current_state = np.array([_OPEN, _CLOSE, _HIGH, _LOW, _VOLUME, _MONEY])
+        self.current_state = None
         self.history = []
 
+    @abstractmethod
     def get_action_space(self):
-        return self.action_space
+        pass
 
+    @abstractmethod
     def get_state_space(self):
-        return self.current_state
+        pass
 
     def reset(self):
         self.queries = 0
--- environments/OneDayEnvironment.py
+++ environments/OneDayEnvironment.py
@@ -4,18 +4,28 @@ import random
 import numpy as np
 from environments.AbstractEnvironment import AbstractEnvironment
 
+action_HOLD = 2
+action_SELL = 1
+action_BUY  = 0
+
+
 class OneDayEnvironment(AbstractEnvironment):
 
-    def __init__(self, history, market_cap=0, money=100, delta = 0.01):
+    def __init__(self, history, market_cap=0, money=100, delta = 0.01, inflation = 0.03):
 
         super(OneDayEnvironment, self).__init__()
         self.num_step = 0
         self.market_cap = market_cap
         self.money = money
+        self.stock = 0
         self.initial_money = money
         self.history = copy.copy(history)
         self.last_state = None
         self.delta = delta
+        self.inflation = inflation
+
+        self.action_space = np.array([action_SELL, action_HOLD, action_BUY])
+        self.current_state = self.make_state_from_history(self.num_step)
 
         self.last_date = None
 
@@ -23,6 +33,8 @@ class OneDayEnvironment(AbstractEnvironment):
         self.last_date = None
         self.num_step = 0
         self.money = self.initial_money
+        self.current_state = self.make_state_from_history(self.num_step)
+        return self.current_state
 
     def step(self, action):
 
@@ -33,31 +45,45 @@ class OneDayEnvironment(AbstractEnvironment):
         if self.last_date == None:
             self.last_date = now_date
 
+        prev_money = self.money + self.stock * now_close
         now_date, last_open, last_close, last_high, last_low, last_volume = self.last_state
-        difa = now_close - last_close
-        if np.abs(difa) < self.delta:
-            difa = 0
-        disc = np.sign(difa)
 
-        if action == disc:
-            reward = 1
-        else:
-            reward = -1
-
-        self.money = self.money + action * now_close
+        if action == action_BUY:
+            self.money -= now_close
+            self.stock += 1
+        elif action == action_SELL:
+            self.money += now_close
+            self.stock -= 1
 
         self.num_step+=1
         self.done = False
-        if self.num_step >= len(self.history)-1 or self.last_date != now_date:
+        if self.num_step >= len(self.history)-1 or self.money <= now_open and self.stock <= 0: # or self.last_date != now_date:
             self.done = True
+            self.money *= (1 - self.inflation)
 
         now_date, next_open, next_close, next_high, next_low, next_volume = self.history[self.num_step]
 
-        next_state = np.array([next_open, next_close, next_high, next_low])
+        now_money = self.money + self.stock * next_close
 
-        info = {'money':self.money}
+        reward = np.sign(now_money - prev_money)
+
+        next_state = np.array([next_open, next_close, next_high, next_low, next_volume])
+
+        info = {'money':self.money, 'stock':self.stock, 'next': next_close}
         return reward, next_state, self.done, info
 
-    def sample(self):
+    def get_action_space(self):
+        return len(self.action_space)
 
+    def get_state_space(self):
+        return len(self.make_state_from_history(self.num_step))
+
+    def make_state_from_history(self, i):
+
+        state = []
+        for s in self.history[i][1:]:
+            state.append(float(s))
+        return np.array(state)
+
+    def sample(self):
         return random.choice(self.action_space)
